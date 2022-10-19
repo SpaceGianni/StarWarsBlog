c330f7f43ef2108986b5f11ce5fb07d7957e7e49
Giannina Baccelliere <gbaccellierem@gmail.com>
2022-10-19 14:49:12 -0300

    quite buscador

--- src/componentes/navbar.jsx
+++ src/componentes/navbar.jsx
@@ -65,18 +65,6 @@ const Navbar = () => {
                 })}
             </ul>
           </div>
-
-          <form className="d-flex" role="search">
-            <input
-              className="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button className="btn btn-outline-success" type="submit">
-              Buscar
-            </button>
-          </form>
         </div>
       </div>
     </nav>
